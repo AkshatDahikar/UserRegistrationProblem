3a58063be4529aa54a33552e9289e994a5c3b2b6
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-02 18:07:20 +0530

    Added : Check Last Name

--- src/UserRegistrationProblem/Registration.java
+++ src/UserRegistrationProblem/Registration.java
@@ -1,5 +1,33 @@
 package UserRegistrationProblem;
 
-public class Registration {
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
+public class Registration {
+	 public static void main(String args[]) {
+		 System.out.println("Welcome to User Registration System");
+		 
+		 Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter first name: "); 
+         String firstName = scanner.nextLine();
+
+         if (checkFirstName(firstName)) { 
+             System.out.println("First Name is Correct"); 
+         } else {
+             System.out.println("Incorrect\nFirst Name contains at least 3 char and starts with capital"); 
+         }
+         System.out.print("Enter last name: "); 
+         String lastName = scanner.nextLine();
+
+         if (checkFirstName(lastName)) { 
+             System.out.println("Last Name is Correct"); 
+         } else {
+             System.out.println("Incorrect\nLastirst Name contains at least 3 char and starts with capital"); 
+         }
+         scanner.close();
+	 }
+	 public static boolean checkFirstName(String firstName) { 
+	     String regex = "^[A-Z][a-z]{2,}$";
+	     return Pattern.matches(regex, firstName);
+	 }
 }
