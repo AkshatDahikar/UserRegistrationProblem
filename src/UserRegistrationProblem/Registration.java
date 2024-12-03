8c2a392875cdde785f6d36d484a622eb6b70c8d0
AkshatDahikar <akshatdahikar@gmail.com>
2024-12-03 10:47:10 +0530

    Added : Check Password Rule1

--- src/UserRegistrationProblem/Registration.java
+++ src/UserRegistrationProblem/Registration.java
@@ -8,33 +8,48 @@ public class Registration {
 		 System.out.println("Welcome to User Registration System");
 		 
 		 Scanner scanner = new Scanner(System.in);
-         System.out.print("Enter first name: "); 
+         System.out.print("Enter first name like contains at least 3 char and starts with capital: "); 
          String firstName = scanner.nextLine();
 
          if (checkFirstName(firstName)) { 
              System.out.println("First Name is Correct"); 
          } else {
-             System.out.println("Incorrect\nFirst Name contains at least 3 char and starts with capital"); 
+             System.out.println("Incorrect First Name "); 
          }
-         System.out.print("Enter last name: "); 
+         System.out.print("Enter last name  like contains at least 3 char and starts with capital :: "); 
          String lastName = scanner.nextLine();
 
          if (checkFirstName(lastName)) { 
              System.out.println("Last Name is Correct"); 
          } else {
-             System.out.println("Incorrect\nLastirst Name contains at least 3 char and starts with capital"); 
+             System.out.println("Incorrect Last Name "); 
          }
-         System.out.print("Enter email: "); 
+         System.out.print("Enter email like this format abc.xyz@bl.co.in: "); 
          String email = scanner.nextLine();
 
          if (checkEmail(email)) { 
              System.out.println("Email is Correct"); 
          } else {
-             System.out.println("Incorrect\n Enter like this format abc.xyz@bl.co.in");
+             System.out.println("Incorrect email");
+         }
+         System.out.print("Enter 10 digit mobile No with country code like 91 9234567890 :"); 
+         String mobileNo = scanner.nextLine();
+
+         if (checkMobileNo(mobileNo)) { 
+             System.out.println("MobileNo is Correct"); 
+         } else {
+             System.out.println("Incorrect MobileNo"); 
+         }
+         System.out.print("Enter password like minimum 8 chars and at least 1 uppercase :");
+         String password = scanner.nextLine();
+         if (checkPassword(password)) { 
+             System.out.println("Password is Correct "); 
+         } else {
+             System.out.println("Incorrect Password "); 
          }
          scanner.close();
-	 }
-	 public static boolean checkFirstName(String firstOrLastName) { 
+	}
+	public static boolean checkFirstName(String firstOrLastName) { 
 	     String regex = "^[A-Z][a-z]{2,}$";
 	     return Pattern.matches(regex,firstOrLastName);
 	 }
@@ -42,4 +57,12 @@ public class Registration {
 	        String regex = "^[a-zA-Z0-9]+([._][a-zA-Z0-9]+)*@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$";
 	        return Pattern.matches(regex, email);
 	    }
+	 public static boolean checkMobileNo(String mobileNo) { 
+	        String regex = "^[0-9]{1,3} [0-9]{10}$";
+	        return Pattern.matches(regex, mobileNo); 
+	    }
+	 public static boolean checkPassword(String password) { 
+		 String regex = "^.{8}$";
+	        return Pattern.matches(regex, password);
+}
 }
